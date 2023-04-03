49210b7a6b0661f090dcfb50474477fcaf217406
css0869 <chenshoushu8866@gmail.com>
2023-04-03 12:20:40 -0400

    Update app data cleaning

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.neighbors import NearestNeighbors
 import re
+from streamlit_extras.badges import badge
+from streamlit_extras.app_logo import add_logo
 from PIL import Image
-#from streamlit_extras.colored_header import colored_header
-#from streamlit_extras.customize_running import center_running
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.customize_running import center_running
 
 import Recommendation as rec
 
@@ -13,6 +15,13 @@ import Recommendation as rec
 def get_data():
     df=pd.read_csv('Clean_data')
     return df
+
+def get_list(df,name):
+    idx=df.index[df['Name']==name][0]
+    s=df['Ig_list'][idx]
+    s=s.replace("'","").replace("[","").replace("]","")  
+    l=[x.strip() for x in s.split(",")]
+    return l
 
 
 def show_product(result_df):
@@ -29,16 +38,13 @@ def show_product(result_df):
         st.write('Price: ',result_df['Price'][i])
         st.write('Rating: ',result_df['Rating'][i]," out of 5 stars")
         st.write('Ingredient: ',result_df['Ingredient'][i])
+        #url=result_df['Link'][i]
+        #st.write("Check out this [link](%s)" % url)
         st.text("")
     return
 
-def get_ig_list(df,name):
-    idx=df.index[df['Name']==name][0]
-    l=re.split('[,]',df['Ingredient'][idx].split(".")[0])
-    l=[x.strip().lower().rstrip(".") for x in l  ]
-    l=[x for x in l if not x.startswith(('*',"please","water","aqua","eau","ingredient")) and not x.isdigit()]
-    return l
-        
+
+       
 def load_page():
     st.set_page_config(page_title="Safe Beauty Product Recommendation App")
     page_bg_img = '''
@@ -50,6 +56,8 @@ background-size: cover;
 </style>
 '''
     st.markdown(page_bg_img, unsafe_allow_html=True)
+    add_logo("http://placekitten.com/120/120")
+    #add_logo("Image/bottle_cloud.png", height=300)
     st.header("Find Safe-For-You Beauty Products")
     #st.title("Find Safe-For-You Beauty Products")
     st.sidebar.image('Image/bottle_cloud.png',  use_column_width=True)
@@ -60,14 +68,11 @@ def app():
     
     df=get_data()
     brand = st.sidebar.selectbox('Brand',sorted(df['Brand'].unique()))     
-    prod = st.sidebar.selectbox('Product Name',sorted(df[df['Brand']==brand]['Name']))
-    #option = st.sidebar.selectbox('I like this product',df['Brand']+[": "]+df['Name'])
-    #option=option.split(":")[1].strip()
-    
- 
+    prod = st.sidebar.selectbox('Product Name',sorted(df[df['Brand']==brand]['Name'])) 
     st.sidebar.subheader("But I don't like...")
     #st.sidebar.write("Which ingredient are you sensitive/allergic to?")
-    allergen=st.sidebar.selectbox("Which ingredient are you sensitive/allergic to?", sorted(get_ig_list(df,prod)))
+    igs=get_list(df,prod)
+    allergen=st.sidebar.selectbox("Which ingredient are you sensitive/allergic to?", options=igs )
     #allergen = st.sidebar.radio("Ingredient List",get_ig_list(df,name) )
     
     btn=st.sidebar.button("Search for Skin Safe Substitutes")
@@ -76,10 +81,10 @@ def app():
     # Create a text element and let the reader know the data is loading.
     if btn:
         data_load_state = st.text('Looking for your new favourites.🏃‍♂️')
-        #center_running()
+        center_running()
         result=rec.recommender(df,prod,allergen)
         st.markdown("You don't like {} . We got you....".format(allergen))
-        st.markdown("These products include similar ingredients as **{}** **{}**, but without **{}** as you noted.".format(brand,prod,allergen)) 
+        st.markdown("These products are similar to **{}** **{}**, but do not contain **{}** as you noted.".format(brand,prod,allergen)) 
         show_product(result)
         # Notify the reader that the data was successfully loaded.
         data_load_state.text('Looking for your new favourites...Found it!')
