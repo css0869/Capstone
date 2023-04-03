4e7ca70e30e22eff9139328ccbd6dda6a3551bed
css0869 <112988319+css0869@users.noreply.github.com>
2023-04-03 14:06:44 -0400

    Update Recommendation.py

--- Recommendation.py
+++ Recommendation.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import numpy as np
+
 from sklearn import base
-from sklearn.feature_extraction import DictVectorizer
+
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.neighbors import NearestNeighbors
-from sklearn.pipeline import Pipeline,FeatureUnion
+
 import re
 
 
